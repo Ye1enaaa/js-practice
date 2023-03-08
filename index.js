55d11dec0156472bd47ea0d027f655f5503cb638
unknown <avilasonson@gmail.com>
2023-03-08 16:33:18 +0800

    problem solved

--- index.js
+++ index.js
@@ -6,14 +6,52 @@ function changeThis(){
 }
 
 function changeDate(){
-    const date = new Date('2023-12-1');
-    document.getElementById('date').innerHTML=date;
+    let date = new Date();
+    let year= date.setFullYear(2023, 12, 1);
+    document.getElementById('date').innerHTML= date;
 }
 
 function addMe(){
     let firstNum = parseInt(document.getElementById('fNum').value);
     let secondNum =parseInt(document.getElementById('sNum').value);
     let result = firstNum + secondNum;
-    console.log(result);
     document.getElementById('result').innerHTML = result;
 }
+
+function subtractMe(){
+    let firstNum = parseInt(document.getElementById('fNum').value);
+    let secondNum =parseInt(document.getElementById('sNum').value);
+    let result = firstNum - secondNum;
+    document.getElementById('result').innerHTML = result;
+}
+
+function multiplyMe(){
+    let firstNum = parseInt(document.getElementById('fNum').value);
+    let secondNum =parseInt(document.getElementById('sNum').value);
+    let result = firstNum * secondNum;
+    document.getElementById('result').innerHTML = result;
+}
+
+function divideMe(){
+    let firstNum = parseInt(document.getElementById('fNum').value);
+    let secondNum =parseInt(document.getElementById('sNum').value);
+    let result = firstNum / secondNum;
+    document.getElementById('result').innerHTML = result;
+}
+
+function randomNumberGen(){
+    let rndomGen = Math.floor(Math.random()*10);
+
+    document.getElementById('randomGen').innerHTML= rndomGen;
+}
+
+function yes(){
+    let yes = document.getElementById('yes').value;
+    document.getElementById('welcome').innerHTML = 'Welcome'
+}
+
+function no(){
+    let no = document.getElementById('no').value;
+    document.getElementById('welcome').innerHTML = 'Sorry'
+}
+
