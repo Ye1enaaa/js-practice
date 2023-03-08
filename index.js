138e0fc2368fe37d7cb2cd865a8586d089484f4d
unknown <avilasonson@gmail.com>
2023-03-08 15:28:50 +0800

    added add function

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ function changeThis(){
     z = x + y;
     document.getElementById('head').innerHTML='Hello Dear';
 }
+
+function changeDate(){
+    const date = new Date('2023-12-1');
+    document.getElementById('date').innerHTML=date;
+}
+
+function addMe(){
+    let firstNum = parseInt(document.getElementById('fNum').value);
+    let secondNum =parseInt(document.getElementById('sNum').value);
+    let result = firstNum + secondNum;
+    console.log(result);
+    document.getElementById('result').innerHTML = result;
+}
